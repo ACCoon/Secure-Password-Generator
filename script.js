e7480d14125cc7856e71c6d0844b85fc114dcbee
Andrew Coon <thenran@gmail.com>
2021-02-16 00:08:33 -0500

    Added function to generate character pool for password generation.

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Generate and return the password pool
+function generatePassPool() {
+  var passPool = "";
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+
+  while(passPool === "") {
+
+    // Ask what types of characters to use in the password, and add chosen characters to password pool
+    if(window.confirm("Do you want to use numbers?")) {
+      passPool += "0123456789";
+    }
+
+    if(window.confirm("Do you want to use lowercase letters?")) {
+      passPool += letters;
+    }
+
+    if(window.confirm("Do you want to use uppercase letters?")) {
+      passPool +=letters.toUpperCase();
+    }
+    
+    if(window.confirm("Do you want to use special characters?")) {
+      passPool += "!@#$%^&*()-_=+`~?/\\\'\"";
+    }
+
+    // If no character types are chosen, notify user before looping to prompt for character types again
+    if(passPool === "") {
+      window.alert("You need to pick at least one set of characters to use for your password!");
+    }
+  }
+  
+  return passPool;
+}
+
+// Generate the password
+function generatePassword() {
+  var password;
+  var passPool = generatePassPool();
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
