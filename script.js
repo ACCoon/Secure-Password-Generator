f1df077973a7ab8391f0bc4b3ff776b98de18efc
Andrew Coon <thenran@gmail.com>
2021-02-16 01:07:20 -0500

    Completed function to generate password from pool

--- script.js
+++ script.js
@@ -36,8 +36,31 @@ function generatePassPool() {
 
 // Generate the password
 function generatePassword() {
-  var password;
+  var passLength;
+  var password = "";
+
+  // Get and validate passLength, if invalid then loop
+  do {
+    // Prompt for length of the password to generate, parse as Integer
+    passLength = parseInt(window.prompt("How long do your want your password to be? It must be between 8 and 128 characters long.", 8));
+
+    // Show alert warning if passLength is not a number between 8 and 128
+    if(isNaN(passLength) || passLength < 8 || passLength > 128){
+      window.alert("You must enter a number between 8 and 128.");
+    }
+
+  } while(isNaN(passLength) || passLength < 8 || passLength > 128);
+  
+  // Call to generate character pool for password
   var passPool = generatePassPool();
+
+  // Generate password following given criteria
+  for (i = 0; i < passLength; i++){
+    password += passPool[Math.floor(Math.random() * (passPool.length - 1))];
+  }
+
+  // Return generated password
+  return password;
 }
 
 // Write password to the #password input
